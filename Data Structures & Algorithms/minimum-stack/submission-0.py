c51089de2f8274c6a06b5101359609450b4ba185
y8ngspider <166143248+y8ngspider@users.noreply.github.com>
2026-05-19 12:16:18 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        
+    def top(self) -> int:
+        return self.stack[-1]
+        
+    def getMin(self) -> int:
+        min = self.stack[-1]
+        for i in range(len(self.stack)):
+            if (self.stack[i]<min):
+                min = self.stack[i]
+        return min
