b5e0f93a27c6b270708c6f84fe61cb78e9cafd61
y8ngspider <166143248+y8ngspider@users.noreply.github.com>
2026-06-29 16:25:19 +0800

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        def helper(i, nums):
+            if i == len(nums):
+                return [[]]
+            
+            resPerms = []
+            perms = helper(i+1, nums)
+            for p in perms:
+                for j in range(len(p)+1):
+                    pCopy = p.copy()
+                    pCopy.insert(j,nums[i])
+                    resPerms.append(pCopy)
+            return resPerms
+        return helper(0, nums)
+
