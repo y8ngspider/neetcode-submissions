e748fa0d7b81e2473691e188dbaf3a7df21a5477
y8ngspider <166143248+y8ngspider@users.noreply.github.com>
2026-07-07 17:55:02 +0800

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # prerequisites -> adjacency list
+        # 2 hashsets, 1 for path and 1 for visited
+
+        # Topo sort
+        # run dfs on each node in adj list
+        # Build adj list
+        adj = {}
+        for i in range(0, numCourses):
+            adj[i] = []
+        for dst, src in prerequisites:
+            adj[src].append(dst)
+        print(adj)
+        path = set()
+        visit = set()
+        # pass src node, path, visit, adj list
+        def dfs(src, path, visit, adj):
+            if src in path:
+                return False
+            
+            visit.add(src)
+            path.add(src)
+            for neighbor in adj[src]:
+                if not dfs(neighbor, path, visit, adj):
+                    return False
+            path.remove(src)
+            return True
+
+        for i in range(numCourses):
+            if not(dfs(i, path, visit, adj)):
+                return False
+        return True
