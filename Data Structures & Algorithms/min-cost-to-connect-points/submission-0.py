f0edeb99e628d7b9cffcad4b043ccd827e5d1511
y8ngspider <166143248+y8ngspider@users.noreply.github.com>
2026-07-11 17:07:16 +0800

    Update: min-cost-to-connect-points - submission-0

--- Data Structures & Algorithms/min-cost-to-connect-points/submission-0.py
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-0.py
@@ -2,47 +2,50 @@ import heapq
 class UnionFind:
     def __init__(self, n):
         self.par = {}
-        self.rank =  {}
+        self.rank = {}
         for i in range(n):
             self.par[i] = i
             self.rank[i] = 0
-        
+
     def find(self, x):
         while x != self.par[x]:
             self.par[x] = self.par[self.par[x]]
             x = self.par[x]
         return x
-
-    def union(self, x, a):
-        px = self.find(x)
+    
+    def union(self, a, b):
         pa = self.find(a)
-        if px == pa:
+        pb = self.find(b)
+
+        if pa == pb:
             return False
-        if self.rank[px] > self.rank[pa]:
-            self.par[pa] = px
-        elif self.rank[pa] > self.rank[px]:
-            self.par[px] = pa
+        if self.rank[pa] > self.rank[pb]:
+            self.par[pb] = pa
+        elif self.rank[pb] > self.rank[pa]:
+            self.par[pa] = pb
         else:
-            self.par[px] = pa
-            self.rank[pa] += 1
-        return True
+            self.par[pa] = pb
+            self.rank[pb] +=1
+        return True    
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         minheap = []
-        n = len(points)
-        for i in range(n):
-            for j in range(i + 1, n):
-                xi, yi = points[i]
-                xj, yj = points[j]
-                dist = abs(xi-xj) + abs(yi-yj)
+        for i in range(len(points)):
+            for j in range(i+1, len(points)):
+                dist = abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1])
                 heapq.heappush(minheap, (dist, i, j))
-        unionFind = UnionFind(n)
-        mst_edges = 0
+        unionFind = UnionFind(len(points))
+        mst = 0
         cost = 0
-        while mst_edges < n - 1:
-            dist, p1, p2 = heapq.heappop(minheap)
-            if unionFind.union(p1, p2):
-                cost += int(dist)
-                mst_edges += 1
+        while mst < len(points) -1:
+            d, n1, n2 = heapq.heappop(minheap)
+            if not unionFind.union(n1,n2):
+                continue
+            mst+=1
+            cost+=d
         return cost
+
+
+
+        
